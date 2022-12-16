390da10b677b39499417ed6dfff0bbb585d9aa8f
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2022-12-16 17:17:02 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const gifts = [];
+function writeCards(gifts, surprise) {
+    for (let i = 0; i < gifts.length; i++) {
+        gifts[i] = `Thank you, ${gifts[i]}, for the wonderful ${surprise} gift!`;
+    }
+    return gifts;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+        debugger;
+    }
+}
+countDown(4)
